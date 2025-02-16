6b9585d46e94f57f12b9a160d4046b206651c3aa
lilmeanieee <rhuwie.1delacuesta@gmail.com>
2025-02-16 19:25:13 +0800

    git commit -m "modified homepage navbar"

--- js/slider.js
+++ js/slider.js
@@ -20,3 +20,12 @@
 
     // Initialize the slider
     showSlide(slideIndex);
+
+    function toggleMenu() {
+        var menu = document.getElementById("mobileMenu");
+        if (menu.style.display === "block") {
+            menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+    }
